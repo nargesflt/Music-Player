6428784c8ad4d736f54cae5ea58aeeaccafef10f
narges <narges.falahat16@gmail.com>
2024-02-17 01:52:11 +0330

    add files

---
+++ js/playlist.js
@@ -0,0 +1,69 @@
+let songs = [
+    {
+        name : "Jamal Jamalou",
+        artist : "Girls' choir",
+        img : "img1",
+        audio : "music1"
+    },
+    {
+        name : "Dokhtare Mashreghiam Man",
+        artist : "Girls' choir",
+        img : "img2",
+        audio : "music2"
+    },
+    {
+        name : "Shokoufeha",
+        artist : "Girls' choir",
+        img : "img3",
+        audio : "music3"
+    },
+    {
+        name : "Gole Aaftab Gardoun",
+        artist : "Girls' choir",
+        img : "img4",
+        audio : "music4"
+    },
+    {
+        name : "Eshghe To Mesle Taghe Boloure",
+        artist : "Girls' choir",
+        img : "img5",
+        audio : "music5"
+    },
+    {
+        name : "Gorg o Bare",
+        artist : "Girls' choir",
+        img : "img6",
+        audio : "music6"
+    },
+    {
+        name : "Leila",
+        artist : "Girls' choir",
+        img : "img7",
+        audio : "music7"
+    },
+    {
+        name : "Banafshe",
+        artist : "Girls' choir",
+        img : "img8",
+        audio : "music8"
+    },
+    {
+        name : "Bachegiamo Yade Man Miare",
+        artist : "Girls' choir",
+        img : "img9",
+        audio : "music9"
+    },
+    {
+        name : "Shab Boud",
+        artist : "Girls' choir",
+        img : "img10",
+        audio : "music10"
+    },
+    {
+        name : "Kalagha",
+        artist : "Girls' choir",
+        img : "img11",
+        audio : "music11"
+    }
+    
+]
