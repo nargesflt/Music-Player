d3c6a2e7f6a686bfaefc1065b4bb4c94af91e2cb
narges <narges.falahat16@gmail.com>
2024-02-18 02:04:29 +0330

    edit code

--- js/playlist.js
+++ js/playlist.js
@@ -64,6 +64,132 @@ let songs = [
         artist : "Girls' choir",
         img : "img11",
         audio : "music11"
+    },
+    {
+        name : "Khosh Aan Rouzi",
+        artist : "Girls' choir",
+        img : "img12",
+        audio : "music12"
+    },
+    {
+        name : "Goli Az Shahre Shiraz",
+        artist : "Girls' choir",
+        img : "img13",
+        audio : "music13"
+    },
+    {
+        name : "Be Didane Man Bia Mahtab Daroumad",
+        artist : "Girls' choir",
+        img : "img14",
+        audio : "music14"
+    },
+    {
+        name : "Sar Ta Pat Ghashange",
+        artist : "Girls' choir",
+        img : "img15",
+        audio : "music15"
+    },
+    {
+        name : "Aay Banoo",
+        artist : "Girls' choir",
+        img : "img16",
+        audio : "music16"
+    },
+    {
+        name : "Mehrabounit Ghashange",
+        artist : "Girls' choir",
+        img : "img17",
+        audio : "music17"
+    },
+    {
+        name : "Rashid Khan",
+        artist : "Girls' choir",
+        img : "img18",
+        audio : "music18"
+    },
+    {
+        name : "Raftam Ke Raftam",
+        artist : "Girls' choir",
+        img : "img19",
+        audio : "music19"
+    },
+    {
+        name : "Doune Doune",
+        artist : "Girls' choir",
+        img : "img20",
+        audio : "music20"
+    },
+    {
+        name : "Gisou Kamande",
+        artist : "Girls' choir",
+        img : "img21",
+        audio : "music21"
+    },
+    {
+        name : "Khoda Mehraboune",
+        artist : "Girls' choir",
+        img : "img22",
+        audio : "music22"
+    },
+    {
+        name : "Mara Ziba Parasti",
+        artist : "Girls' choir",
+        img : "img23",
+        audio : "music23"
+    },
+    {
+        name : "Porsoun Porsoun",
+        artist : "Girls' choir",
+        img : "img24",
+        audio : "music24"
+    },
+    {
+        name : "Sabrom Dige Sar Rafte",
+        artist : "Girls' choir",
+        img : "img25",
+        audio : "music25"
+    },
+    {
+        name : "Yaram Moshkel Pasande",
+        artist : "Girls' choir",
+        img : "img26",
+        audio : "music26"
+    },
+    {
+        name : "GolPari Joun",
+        artist : "Girls' choir",
+        img : "img27",
+        audio : "music27"
+    },
+    {
+        name : "Dokhtare BoyerAhmadi",
+        artist : "Girls' choir",
+        img : "img28",
+        audio : "music28"
+    },
+    {
+        name : "Aghaghi",
+        artist : "Girls' choir",
+        img : "img29",
+        audio : "music29"
+    },
+    {
+        name : "A Solmaz",
+        artist : "Girls' choir",
+        img : "img30",
+        audio : "music30"
+    },
+    {
+        name : "Delam Mikhad",
+        artist : "Girls' choir",
+        img : "img31",
+        audio : "music31"
+    },
+    {
+        name : "Ghalbe Man",
+        artist : "Girls' choir",
+        img : "img32",
+        audio : "music32"
     }
     
 ]
